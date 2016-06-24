365fc934ad0aad7f2dab1a2334f05bbdf0fd368d
Thamme Gowda <thammegowda@apache.org>
2016-06-24 14:02:28 -0700

    Added a bash script to execute the sparkler

---
+++ bin/sparkler.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$DIR/.."
+JAR=`ls $DIR/sparkler-app/target/sparkler-app-*-SNAPSHOT.jar`
+if [ ! -f "$JAR" ]
+ then
+    echo "Cant find Jar. Perhaps the sources are not built to produce a jar?"
+    exit 2
+fi
+
+# run
+java -cp $DIR/conf:$JAR \
+    edu.usc.irds.sparkler.Main $@
