b50bcf3272f0d9a954aa4e8b05f3bde6ec826dbd
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-10-23 17:05:46 -0700

    Removed CloseableIterator interface added earlier

--- sparkler-api/src/main/java/edu/usc/irds/sparkler/CloseableIterator.java
+++
@@ -1,9 +0,0 @@
-package edu.usc.irds.sparkler;
-
-import java.util.Iterator;
-
-/**
- * Created by karanjeetsingh on 10/23/16.
- */
-public interface CloseableIterator<T> extends Iterator<T>, AutoCloseable {
-}
