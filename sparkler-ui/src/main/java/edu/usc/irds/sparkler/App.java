202a5e6908097450cca7d1c6316b48a2eaf98db7
Karanjeet Singh <connect.karanjeet@gmail.com>
2016-10-24 14:35:58 -0700

    Added sparkler-ui for Banana Dashboard

---
+++ sparkler-ui/src/main/java/edu/usc/irds/sparkler/App.java
@@ -0,0 +1,13 @@
+package edu.usc.irds.sparkler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
