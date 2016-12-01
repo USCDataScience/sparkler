986d4b7cbf7f88bbd8f5a470c8845709014b910b
Tom Barber <tom@analytical-labs.com>
2016-12-01 19:59:09 +0000

    update boilerplate

--- sparkler-deployment/juju/sparkler/reactive/sparkler.py
+++ sparkler-deployment/juju/sparkler/reactive/sparkler.py
@@ -3,15 +3,27 @@ from charms.reactive import when, when_not, set_state
 
 @when_not('sparkler.installed')
 def install_sparkler():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
     set_state('sparkler.installed')
+
+@when_not('java.ready')
+def no_java():
+
+@when_not('spark.joined')
+def no_spark():
+
+@when_not('spark.started')
+def spark_not_started():
+
+@when_not('solr.joined')
+def no_solr():
+
+@when_not('solr.started'):
+def solr_not_started():
+
+@when('solr.started')
+@when('spark.started')
+@when('java.ready')
+def configure_sparkler():
+
+@when('sparkler.configured')
+def run_sparkler():
