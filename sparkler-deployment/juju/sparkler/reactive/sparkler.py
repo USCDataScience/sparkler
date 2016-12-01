3fad53f9374e794aac3239ce69a6e704595644ff
Tom Barber <tom@analytical-labs.com>
2016-12-01 19:29:14 +0000

    bootstrap code for sparkler juju charm

---
+++ sparkler-deployment/juju/sparkler/reactive/sparkler.py
@@ -0,0 +1,17 @@
+from charms.reactive import when, when_not, set_state
+
+
+@when_not('sparkler.installed')
+def install_sparkler():
+    # Do your setup here.
+    #
+    # If your charm has other dependencies before it can install,
+    # add those as @when() clauses above., or as additional @when()
+    # decorated handlers below
+    #
+    # See the following for information about reactive charms:
+    #
+    #  * https://jujucharms.com/docs/devel/developer-getting-started
+    #  * https://github.com/juju-solutions/layer-basic#overview
+    #
+    set_state('sparkler.installed')
