cf0a85c2709384e99ccf6e466aab67df85113e7f
Tom Barber <tom@spicule.co.uk>
2019-03-21 00:18:03 +0000

    add chrome driver for sparkler text extraction. fixes #173

---
+++ sparkler-plugins/fetcher-chrome/src/main/java/edu/usc/irds/sparkler/plugin/FetcherChromeActivator.java
@@ -0,0 +1,14 @@
+package edu.usc.irds.sparkler.plugin;
+
+import org.pf4j.Plugin;
+import org.pf4j.PluginWrapper;
+
+/**
+ * Plugin Activator for start and stop call back
+ */
+public class FetcherChromeActivator extends Plugin {
+
+    public FetcherChromeActivator(PluginWrapper wrapper) {
+        super(wrapper);
+    }
+}
