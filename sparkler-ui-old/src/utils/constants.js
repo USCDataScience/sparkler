68ae6f79fb12dbc0bfde999e3af6e279641062e1
Tom Barber <tom@spicule.co.uk>
2020-09-25 16:43:42 +0100

    fix base url

--- sparkler-ui-old/src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-import 'location-origin';
-
-dotenv.config();
-
-export const ROUTER_BASENAME = process.env.REACT_APP_ROUTER_BASENAME;
-
-export const API_URL = process.env.REACT_APP_API_URL
