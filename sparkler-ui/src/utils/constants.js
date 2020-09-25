18fafaaa327861305b18d19bb3d1a752a7f5b145
Tom Barber <tom@spicule.co.uk>
2020-09-25 15:48:45 +0100

    fix base url

--- sparkler-ui/src/utils/constants.js
+++ sparkler-ui/src/utils/constants.js
@@ -5,4 +5,9 @@ dotenv.config();
 
 export const ROUTER_BASENAME = process.env.REACT_APP_ROUTER_BASENAME;
 
-export const API_URL = process.env.REACT_APP_API_URL
+if(process.env.REACT_APP_API_URL) {
+    export const API_URL = process.env.REACT_APP_API_URL
+}
+else {
+    export const API_URL = "/"
+}
