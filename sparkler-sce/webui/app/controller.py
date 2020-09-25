505f24c2288fde6b3a33760069c7f663dfdcbc67
Tom Barber <tom@spicule.co.uk>
2020-09-25 17:46:16 +0100

    update network

--- sparkler-sce/webui/app/controller.py
+++ sparkler-sce/webui/app/controller.py
@@ -206,8 +206,8 @@ def start_crawl(model):
     print('Pulling latest')
     qcmd = ['sudo', 'docker', 'pull', 'uscdatascience/sparkler:latest']
     subprocess.Popen(qcmd)
-    print('Running container') #'--network', 'compose_default',
-    qcmd = ['sudo', 'docker', 'run', '--name', model + 'crawl',
+    print('Running container')
+    qcmd = ['sudo', 'docker', 'run', '--network', 'sparkler', '--name', model + 'crawl',
             'uscdatascience/sparkler:latest'] + cmd
     subprocess.Popen(qcmd)
     return 'crawl started'
