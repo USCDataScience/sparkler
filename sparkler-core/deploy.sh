b8287277c46d1c88bb281e7963239cfb796dc56c
Tom Barber <tom@spicule.co.uk>
2021-06-29 12:59:48 +0100

    new sbt changes

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -3,6 +3,6 @@
 pip install databricks-cli
 
 rm -rf build/sparkler-app-0.3.1-SNAPSHOT
-rm -rf build/plugins
+#rm -rf build/plugins
 
 ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
