f2d8c8ad6dc482085c9026c51c0c060d43087e6d
Tom Barber <tom@spicule.co.uk>
2021-06-30 21:15:53 +0100

    build both versions

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+standalone=$1
+
 pip install databricks-cli
 
-rm -rf build/sparkler-app-0.3.1-SNAPSHOT
-#rm -rf build/plugins
+if [ "$standalone" = "true" ]; then
+  ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-standalone/
+else
+  rm -rf build/sparkler-app-0.3.1-SNAPSHOT
 
-~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
+  ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
+fi
