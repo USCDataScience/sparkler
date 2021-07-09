9aed461c170c62bffd8c91a0986d57ec528e6c16
Tom Barber <tom@spicule.co.uk>
2021-07-09 21:48:40 +0100

    update packaging

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -5,10 +5,11 @@ method=$1
 pip install databricks-cli
 
 if [ "$method" = "standalone" ]; then
-  ls
+  rm -rf build/sparkler-app-0.3.1-SNAPSHOT
+  mv build sparkler
+  zip -r sparkler.zip sparkler
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-standalone/
 else
   rm -rf build/sparkler-app-0.3.1-SNAPSHOT
-
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
 fi
