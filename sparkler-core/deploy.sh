ad4864d4076c56008d71e80fae5f32489889d9e7
Tom Barber <tom@spicule.co.uk>
2021-07-09 21:56:04 +0100

    update packaging

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -8,7 +8,7 @@ if [ "$method" = "standalone" ]; then
   rm -rf build/sparkler-app-0.3.1-SNAPSHOT
   mv build sparkler
   zip -r sparkler.zip sparkler
-  ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-standalone/
+  ~/.local/bin/databricks fs cp --recursive --overwrite sparkler.zip dbfs:/FileStore/sparkler-standalone/
 else
   rm -rf build/sparkler-app-0.3.1-SNAPSHOT
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
