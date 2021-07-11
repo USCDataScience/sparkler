f6c07caa1fc3e388fdf45895551288c46557f16b
Tom Barber <tom@spicule.co.uk>
2021-07-11 18:39:16 +0100

    change params to parameters

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -9,23 +9,23 @@ if [ "$method" = "standalone" ]; then
   mv build sparkler
   zip -r sparkler.zip sparkler
   export DATABRICKS_HOST=$DEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$DEV_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$DEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite sparkler.zip dbfs:/FileStore/sparkler-standalone/
   export DATABRICKS_HOST=$NEWDEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$NEWDEV_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$NEWDEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite sparkler.zip dbfs:/FileStore/sparkler-standalone/
   export DATABRICKS_HOST=$TEST_DATABRICKS_HOST
-  export DATABRICKS_KEY=$TEST_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$TEST_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite sparkler.zip dbfs:/FileStore/sparkler-standalone/
 else
   rm -rf build/sparkler-app-0.3.1-SNAPSHOT
   export DATABRICKS_HOST=$DEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$DEV_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$DEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
   export DATABRICKS_HOST=$NEWDEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$NEWDEV_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$NEWDEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
   export DATABRICKS_HOST=$TEST_DATABRICKS_HOST
-  export DATABRICKS_KEY=$TEST_DATABRICKS_TOKEN
+  export DATABRICKS_TOKEN=$TEST_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
 fi
