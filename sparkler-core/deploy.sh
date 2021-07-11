5451b7bc72cc80786683395898048ff18c9e3289
Tom Barber <tom@spicule.co.uk>
2021-07-11 18:33:24 +0100

    change params to parameters

--- sparkler-core/deploy.sh
+++ sparkler-core/deploy.sh
@@ -9,7 +9,7 @@ if [ "$method" = "standalone" ]; then
   mv build sparkler
   zip -r sparkler.zip sparkler
   export DATABRICKS_HOST=$DEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$DEV_DATABRICKS_KEY
+  export DATABRICKS_KEY=$DEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite sparkler.zip dbfs:/FileStore/sparkler-standalone/
   export DATABRICKS_HOST=$NEWDEV_DATABRICKS_HOST
   export DATABRICKS_KEY=$NEWDEV_DATABRICKS_TOKEN
@@ -20,7 +20,7 @@ if [ "$method" = "standalone" ]; then
 else
   rm -rf build/sparkler-app-0.3.1-SNAPSHOT
   export DATABRICKS_HOST=$DEV_DATABRICKS_HOST
-  export DATABRICKS_KEY=$DEV_DATABRICKS_KEY
+  export DATABRICKS_KEY=$DEV_DATABRICKS_TOKEN
   ~/.local/bin/databricks fs cp --recursive --overwrite build/ dbfs:/FileStore/sparkler-submit/
   export DATABRICKS_HOST=$NEWDEV_DATABRICKS_HOST
   export DATABRICKS_KEY=$NEWDEV_DATABRICKS_TOKEN
