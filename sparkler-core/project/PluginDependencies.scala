56fd55b4e1d113f686ecd812293ff694383dfea4
Tom Barber <tom@spicule.co.uk>
2021-07-13 11:43:18 +0100

    update scripter

--- sparkler-core/project/PluginDependencies.scala
+++ sparkler-core/project/PluginDependencies.scala
@@ -28,7 +28,7 @@ object FetcherChrome {
     lazy val java = group % "selenium-java" % version
   }
   lazy val browserup = "com.browserup" % "browserup-proxy-core" % "3.0.0-SNAPSHOT"
-  lazy val seleniumscripter = "com.kytheralabs" % "seleniumscripter" % "1.4-20210712.093510-6"
+  lazy val seleniumscripter = "com.kytheralabs" % "seleniumscripter" % "1.5"
 }
 
 object FetcherHtmlUnit {
