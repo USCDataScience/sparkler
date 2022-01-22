b7a4e551b1452905a4af1414d0f846445d3bdf35
Tom Barber <tom@spicule.co.uk>
2022-01-22 18:29:58 +0000

    update tika version (#246)

--- sparkler-core/bin/sparkler.sh
+++ sparkler-core/bin/sparkler.sh
@@ -25,7 +25,7 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-<<<<<<< HEAD
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 SPARKLER_BUILD_DIR="$DIR/../build"
 JAR=`echo $DIR/../sparkler-app-*/lib`
